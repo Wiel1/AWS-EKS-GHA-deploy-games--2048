5664ca5be2775fa6e52f06ada91429058566d420
techoutcomes <127057485+techoutcomes@users.noreply.github.com>
2023-08-27 17:40:10 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
-ww
+resource "kubectl_manifest" "games_2048_namespace" {
+    yaml_body = file("${path.module}/config/games-2048/2048_namespace_v254.yaml")
+}
+
+resource "kubectl_manifest" "games_2048_deployment" {
+    yaml_body = file("${path.module}/config/games-2048/2048_deployment_v254.yaml")
+}
+
+resource "kubectl_manifest" "games_2048_service" {
+    yaml_body = file("${path.module}/config/games-2048/2048_service_v254.yaml")
+}
+
+resource "kubectl_manifest" "games_2048_ingress" {
+    yaml_body = file("${path.module}/config/games-2048/2048_ingress_v254.yaml")
+}
