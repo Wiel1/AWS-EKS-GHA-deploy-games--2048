b532030063b60319c3cdefa0576f994daed3a283
techoutcomes <127057485+techoutcomes@users.noreply.github.com>
2023-08-27 18:28:32 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,13 +18,13 @@ terraform {
 }
 
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  host                   = data.terraform_remote_state.remote.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.remote.cluster_cert)
   load_config_file       = false
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    args        = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.remote.cluster_id]
     command     = "aws"
   }
 }
