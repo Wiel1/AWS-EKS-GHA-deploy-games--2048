27db9155280b7f8815005fb80650cdbdd3c7985b
techoutcomes <127057485+techoutcomes@users.noreply.github.com>
2023-08-27 17:31:06 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,19 @@
-ksks
+provider "aws" {
+  region = "eu-west-1"
+}
+
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.6.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
